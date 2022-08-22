6458e21fdddcab47db8d8e7f5f748cb691c3c0a5
Blues Yu <bluesedenyu@gmail.com>
2022-08-22 17:37:26 +0800

    feature: 增加 faulthandler 支持更友好的 Gunicorn worker 错误堆栈 #27

--- {{cookiecutter.project_name}}/src/api/{{cookiecutter.api_name}}/wsgi.py
+++ {{cookiecutter.project_name}}/src/api/{{cookiecutter.api_name}}/wsgi.py
@@ -6,6 +6,11 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
+import faulthandler
+
+# 开启后可以更有效地捕获 Gunicorn worker timeout 问题的堆栈信息
+# https://stackoverflow.com/questions/57167240/is-it-possible-to-get-a-stack-trace-when-a-gunicorn-worker-hits-a-timeout
+faulthandler.enable()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', '{{cookiecutter.api_name}}.settings.overlays.prod')
 
